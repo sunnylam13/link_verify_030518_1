f311353182e9ef023d3b53e51035b382d9149292
Sunny Lam <sunny.lam@gmail.com>
2018-03-05 20:51:53 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Link Verification
+
+A program that tries to download every linked page on the page of a given URL.  It flags any pages with the 404 "Not Found" status code and prints them out as broken links.
+
+## References
+
+ABSP:  440
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description': 'A program that tries to download every linked page on the page of a given URL.  It flags any pages with the 404 "Not Found" status code and prints them out as broken links.',
+	'author': 'My Name',
+	'url': 'URL to get it at',
+	'download_url': 'Where to download it',
+	'author_email': 'sunny.lam@gmail.com',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['NAME'],
+	'scripts': [],
+	'name': 'Link Verification'
+}
+
+setup(**config)
